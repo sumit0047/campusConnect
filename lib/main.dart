7d1778d9707977528eda2282cc22d7f08fb61395
Sumit <sumit.kr445@gmail.com>
2020-12-02 21:36:40 +0530

    added login/landing navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,31 @@
 import 'package:campus_connect/screens/Landing.dart';
+import 'package:campus_connect/screens/Login.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: LandingScreen(),
-    );
+
+          if (FirebaseAuth.instance.currentUser!=null){
+            /// user already logged in
+            return MaterialApp(
+              home: LandingScreen(),
+            );
+          }
+          /// no user logged in
+          return MaterialApp(
+            home: Login(),
+          );
+        }
   }
-}
+
 
