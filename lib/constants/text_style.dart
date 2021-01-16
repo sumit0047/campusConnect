962a60b3d2adba3013392637ad172a321569614f
Sumit <sumit.kr445@gmail.com>
2021-01-16 23:11:29 +0530

    added constants

---
+++ lib/constants/text_style.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const headerStyle = TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold,fontFamily: 'Montserrat');
+const titleStyle = TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold,fontFamily: 'Montserrat');
+const subtitleStyle = TextStyle(color: Colors.white, fontSize: 14,fontFamily: 'Montserrat');
+const monthStyle = TextStyle(color: Colors.orange, fontSize: 16, fontWeight: FontWeight.w500,fontFamily: 'Montserrat');
